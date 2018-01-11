751d320c8651952213411befd80f8a3789635afa
Jonathan Saje <2167936s@student.gla.ac.uk>
2018-01-11 15:57:45 +0000

    First Commit

---
+++ robot_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/rflab/robot_ws/devel:$CMAKE_PREFIX_PATH"
+export PATH="/opt/ros/kinetic/bin:/home/rflab/bin:/home/rflab/.local/bin:/home/rflab/bin:/home/rflab/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+export PWD="/home/rflab/robot_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/rflab/robot_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/rflab/robot_ws/src:$ROS_PACKAGE_PATH"
